c52fadaf9ba96a9431a584990c50d182f159525d
sanjayrprabhu <149789340+sanjayrprabhu@users.noreply.github.com>
2023-11-03 18:21:18 +0530

    Sanjay R Prabhu

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-
+counts={}
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+  for i in present_capacities:
+    soh=100*i/120
+    if soh>=80 and soh<=100:
+      h+=1
+    elif soh>=60 and soh<80:
+      e+=1
+    elif soh<60:
+      f+=1
+  counts={"healthy":h,"exchange":e,"failed":f}
+
+  
+  
 
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
-  counts = count_batteries_by_health(present_capacities)
+  present_capacities = input().split()
+  count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
+  assert(counts["healthy"] == 1)
+  assert(counts["exchange"] ==2)
+  assert(counts["failed"] == 2)
   print("Done counting :)")
 
 
